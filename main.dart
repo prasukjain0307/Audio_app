559b4dfeb4f5bd55851ea1d8a272f38fda205552
prasukjain0307 <66464644+prasukjain0307@users.noreply.github.com>
2020-08-06 16:12:02 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,39 +12,6 @@ class MyApp extends StatelessWidget {
     return  myapp();
   }
   }
-  //    MaterialApp(home: Scaffold(
-  //     appBar: AppBar(
-  //       title:Text('LearnForward'),
-  //       backgroundColor: Colors.redAccent.shade700,
-  //     ),
-  //     body:Row(
-  //       children: <Widget>[
-  //         Container(
-  //           width:200,
-  //           height:150,
-  //           child:Card(
-  //             child:Image.network(src),
-  //             color: Colors.red,
-  //             elevation: 12,
-
-
-  //           ),
-  //         ),
-  //         Container(
-  //           width:100,
-  //           height: 100,
-  //           child:
-  //           Card(
-  //             child:RaisedButton(
-  //             onPressed:()=> print('hii'),
-  //             ),),
-              
-  //         ),
-          
-  //       ],
-  //     ),
-  //   ),
-  // );
-  //}   
+ 
     
   
